b8edbcdbd49dfc9da029e972126fe568b3725ea2
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-16 18:58:02 -0300

    refactor: update url patterns in songs_project

--- songs_project/urls.py
+++ songs_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('songs_app.urls')),
+    path('api/', include('songs_app.urls')),
 ]
