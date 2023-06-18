d59cd6bb503ff3c4195da62aa8f6a39d93b35402
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-18 19:22:08 -0300

    refactor: add viewsets classes for each model, to get, create, update and delete data from B

--- songs_app/views.py
+++ songs_app/views.py
@@ -1,86 +1,59 @@
-import json
-from django.http import JsonResponse
-from rest_framework import viewsets, permissions, status
-from django.core import serializers
-from rest_framework. response import Response
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404, render
-from .models import Song, Artist, Genre
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from songs_app.models import Song, Artist, Genre
+from songs_app.serializers import SongSerializer, ArtistSerializer, GenreSerializer
 
-## CRUD SONGS ##
-def index(request):
-    return JsonResponse({'message': 'Do you like listening to music?'})
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.select_related('artist', 'genre').all()
+    serializer_class = SongSerializer
 
-def get_all_songs(request):
-    all_songs = Song.objects.select_related('artist', 'genre').all()
-    songs_data = []
-    for song in all_songs:
-        song_data = {
-            'id': song.id,
-            'title': song.title,
-            'artist': song.artist.name,
-            'album': song.album,
-            'duration': song.duration,
-            'genre': song.genre.name,
-            'image': song.image,
-            'you_tube': song.you_tube
-        }
-        songs_data.append(song_data)
-    return JsonResponse(songs_data, status=200, safe=False)
+    @action(detail=True, methods=['put'])
+    def update_song(self, request, pk=None):
+        song = self.get_object()
+        serializer = self.get_serializer(song, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-def get_one_song(request, id):
-    song = list(Song.objects.filter(pk=id))
-    if song:
-        return JsonResponse(song, status=200, safe=False)
-    else:
-        return JsonResponse({'message': 'song was not found'}, status=404)
+    @action(detail=True, methods=['delete'])
+    def delete_song(self, request, pk=None):
+        song = self.get_object()
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-@csrf_exempt
-def new_song(request):
-    artist = None
-    genre = None
-    artists_db = Artist.objects.all()
-    genres_db = Genre.objects.all()
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        print(data)
-        for artist_db in artists_db:
-            if artist_db.name == data['artist']:
-                artist = artist_db
-                break
-        for genre_db in genres_db:
-            if genre_db.name == data['genre']:
-                genre = genre_db
-                break
-        song = Song(title=data['title'], artist=artist, album=data['album'], duration=data['duration'], genre=genre, image=data['image'], you_tube=data['you_tube'])
-        song.save()
-        return JsonResponse({'message': 'new song was saved'}, status=200)
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
 
+    @action(detail=True, methods=['put'])
+    def update_artist(self, request, pk=None):
+        artist = self.get_object()
+        serializer = self.get_serializer(artist, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    
+    @action(detail=True, methods=['delete'])
+    def delete_artist(self, request, pk=None):
+        artist = self.get_object()
+        artist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-## CRUD ARTISTS ##
-def get_all_artists(request):
-    all_artists = list(Artist.objects.values())
-    return JsonResponse(all_artists, status=200, safe=False)
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
-@csrf_exempt
-def new_artist(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        print(data)
-        artist = Artist(name=data['name'])
-        artist.save()
-    return JsonResponse({'message': '...'}, status=200)
-
-## CRUD GENRES ##
-def get_all_genres(request):
-    all_genres = list(Genre.objects.values())
-    return JsonResponse(all_genres, status=200, safe=False)
-
-@csrf_exempt
-def new_genre(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        print(data)
-        genre = Genre(name=data['name'])
-        genre.save()
-    return JsonResponse({'message': '...'}, status=200)
+    @action(detail=True, methods=['put'])
+    def update_genre(self, request, pk=None):
+        genre = self.get_object()
+        serializer = self.get_serializer(genre, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    
+    @action(detail=True, methods=['delete'])
+    def delete_genre(self, request, pk=None):
+        genre = self.get_object()
+        genre.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
