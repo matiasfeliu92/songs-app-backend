7f025bb4ca41b451ec6becdcd2eb0188d08f3d1e
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-23 19:23:45 -0300

    refactor: add TypeOrm setings for database in app.module and add api/v1 prefix and cors settings in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { ArtistsModule } from './artists/artists.module';
 import { GenresModule } from './genres/genres.module';
+import { keys } from './keys';
 
 @Module({
-  imports: [SongsModule, ArtistsModule, GenresModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || keys.dbPassword,
+      database: process.env.DB_NAME || 'songs_local',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+    SongsModule,
+    ArtistsModule,
+    GenresModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
