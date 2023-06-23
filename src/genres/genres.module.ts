5d073285149e2123dc85a42c3bd75a4fb73fdec2
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-23 19:25:37 -0300

    refactor: update genres, add service and controllers for get all, get one by id and add entity model

--- src/genres/genres.module.ts
+++ src/genres/genres.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { GenresController } from './controllers/genres.controller';
 import { GenresService } from './services/genres.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Genre } from './entities/genre.entity';
 
+@Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([Genre])],
   controllers: [GenresController],
-  providers: [GenresService]
+  providers: [GenresService],
+  exports: [GenresService],
 })
 export class GenresModule {}
